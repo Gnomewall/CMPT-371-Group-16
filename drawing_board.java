5b2bf6f94d1b7f1fc450aa91fa857ddd33badf3d
Hani <hani.dnguyen@gmail.com>
2023-07-15 14:25:11 -0700

    Editted drawing_board.java

--- drawing_board.java
+++ drawing_board.java
@@ -2,30 +2,49 @@ import javax.swing.*;
 import java.awt.*;
 
 public class drawing_board {
+
+    //REFERENCE:
+    //https://www.ssaurel.com/blog/learn-how-to-make-a-swing-painting-and-drawing-application/
+    DrawArea drawArea;
+
+    //show canvas/frame
+    public void show(){
+        //create main frame
+        //TODO: name the game
+        JFrame frame = new JFrame("TODO: Name The Game!");
+
+        //get frame container
+        Container content = frame.getContentPane();
+        //set layout on content pane
+        content.setLayout(new BorderLayout());
+
+        //set cursor image
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Image image = toolkit.getImage("Cursor image/pencil cartoon.png");
+        Cursor c = toolkit.createCustomCursor(image , new Point(content.getX(),
+                content.getY()), "img");
+        content.setCursor (c);
+
+        //create the drawing area
+        drawArea = new DrawArea();
+
+        //and add to content pane
+        content.add(drawArea,BorderLayout.CENTER);
+
+        //set size of frame
+        frame.setSize(600, 600);
+        // can close frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // show the swing paint result
+        frame.setVisible(true);
+    }
+
     public static void main(String[] args) {
         // might be useful if we need multiple threads
         SwingUtilities.invokeLater(() -> {
-            // adding a jFrame and a title to the drawing board. We can probably add the
-            // game title here
-            JFrame frame = new JFrame("TODO: Name The Game!");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-            JPanel canvas = new JPanel() {
-                @Override
-                protected void paintComponent(Graphics g) {
-                    super.paintComponent(g);
-                    // have to add drawing logic here
-                }
-            };
-
-            // the size of the canvas
-            canvas.setPreferredSize(new Dimension(500, 500));
-
-            frame.getContentPane().add(canvas);
-            // frame sizing
-            frame.pack();
-            // makes the things visible on our canvas
-            frame.setVisible(true);
+            //call show method to open window
+            new drawing_board().show();
         });
     }
 
